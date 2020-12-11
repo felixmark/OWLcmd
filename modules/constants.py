385fc0734a3242e011e9f9edbd47ceee14a76041
Felix <felix.mark@hotmail.com>
2020-12-12 00:19:02 +0100

    A try to fix gunicorn issues.

--- modules/constants.py
+++ modules/constants.py
@@ -1,12 +1,12 @@
 
 class Constants(object):
     WEBSITE_TITLE = "OWLcmd"
-    VERSION = "0.0.8"
+    VERSION = "0.0.9"
 
     MACHINE_NAME = "owl"
     PORT = 80
-    PING_INTERVAL = 5
-    PING_TIMEOUT = 10
+    PING_INTERVAL = 2
+    PING_TIMEOUT = 20
     HOST = '0.0.0.0'
     DEBUG = False
     THREADED = True
