faeb7eebb10b0e166428ba3111a40ff7f62061a8
Felix Mark <felix.mark@hotmail.com>
2020-12-11 17:23:10 +0100

    Minor changes.

--- app.py
+++ app.py
@@ -137,4 +137,4 @@ def favicon():
 
 # =================================================== MAIN ===================================================
 if __name__ == '__main__':
-    socketio.run(app, debug=Constants.DEBUG, port=Constants.PORT)
+    socketio.run(app, host=Constants.HOST, debug=Constants.DEBUG, port=Constants.PORT)
